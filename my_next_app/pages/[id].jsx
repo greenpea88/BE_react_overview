c07e3631b9d889b8f9b530dd81f3dd17798dcd90
greenpea88 <green_pea88@naver.com>
2022-01-26 10:43:20 +0900

    :building_construction: CHANGE: test just page move

--- my_next_app/pages/[id].jsx
+++ my_next_app/pages/[id].jsx
@@ -3,6 +3,12 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 
 export default function indexId(){
+    // return (
+    //     <>
+    //         <div>this is id page</div>
+    //         <Link href={("/")}>back to index page</Link>
+    //     </>
+    // );
     const router = useRouter();
     const {id: indexId} = router.query;
 
