922f36dab3a9921af7522442acd9cfa55e5bdf5e
greenpea88 <green_pea88@naver.com>
2022-01-26 09:20:40 +0900

    :sparkles: NEW: use Link to move page

---
+++ my_next_app/pages/hello.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const HelloPage = () => {
+    return(
+        <React.Fragment>
+            <div>this is hello page</div>
+        </React.Fragment>
+    );
+};
+
+export default HelloPage;
